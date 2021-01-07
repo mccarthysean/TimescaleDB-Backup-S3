768612a5485dfd38f187784ec5cdf25fdb00e87c
Sean McCarthy <sean.mccarthy@hotmail.com>
2021-01-06 20:18:15 -0700

    forgot to disable the docker stack deploy command in the example

--- deploy11.sh
+++ deploy11.sh
@@ -14,9 +14,9 @@ echo "Pushing the image to Docker Hub..."
 echo "docker push mccarthysean/timescaledb_backup_s3:11"
 docker push mccarthysean/timescaledb_backup_s3:11
 
-# Deploy to the Docker swarm and send login credentials 
-# to other nodes in the swarm with "--with-registry-auth"
-echo ""
-echo "Deploying to the server..."
-echo "docker stack deploy --with-registry-auth -c docker-compose.example.yml timescale11"
-docker stack deploy --with-registry-auth -c docker-compose.example.yml timescale11
+# # Deploy to the Docker swarm and send login credentials 
+# # to other nodes in the swarm with "--with-registry-auth"
+# echo ""
+# echo "Deploying to the server..."
+# echo "docker stack deploy --with-registry-auth -c docker-compose.example.yml timescale11"
+# docker stack deploy --with-registry-auth -c docker-compose.example.yml timescale11
