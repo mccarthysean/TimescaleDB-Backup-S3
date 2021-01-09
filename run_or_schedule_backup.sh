8358c2bb558f03fa185130c1196a8e50c46f2a19
Sean McCarthy <sean.mccarthy@hotmail.com>
2021-01-08 17:16:35 -0700

    switching to 'timescale-backup' for 'ts-dump' and 'ts-restore' Golang binaries

--- run_or_schedule_backup.sh
+++ run_or_schedule_backup.sh
@@ -18,7 +18,7 @@ echo "SCHEDULE environment variable = '$SCHEDULE' so we're setting up cron now..
 # Create a crontab.txt file to set as the default crontab
 echo "
 # min hour dom month dow   command
-$SCHEDULE /bin/sh /backup.sh
+$SCHEDULE /bin/sh /backup_ts.sh
 # Leave the last line blank for a valid cron file" > /crontab.txt
 
 # Set the default crontab as the crontab.txt file
